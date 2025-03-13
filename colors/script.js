fb0ef68af537a70205a4c0f718f99ec8e98d328c
Theekshana Nirmal <theekshanab1234@gmail.com>
2025-03-13 10:06:07 +0530

    pick RGB colors

--- colors/script.js
+++ colors/script.js
@@ -1,10 +1,11 @@
-var index = 0;
+// This function generates random red, green, and blue values and uses them to change the background color of the webpage.
 
 function changeColor(){
-  const colors = ["red", "yellow", "green"];
-  document.getElementsByTagName("body")[0].style.backgroundColor = colors[index++];
+  // Generate random color values
+  var red = Math.floor(Math.random() * 256);
+  var green = Math.floor(Math.random() * 256);
+  var blue = Math.floor(Math.random() * 256);
 
-  if(index > colors.length-1){
-    index = 0;
-  }
+  // Apply the random color to the webpage background
+  document.getElementsByTagName("body")[0].style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
 }
