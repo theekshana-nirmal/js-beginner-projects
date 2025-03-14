aa7dc66d2817afa8542af2d0d69f3bd0705b372c
Theekshana Nirmal <theekshanab1234@gmail.com>
2025-03-14 17:13:40 +0530

    Add event listener

--- pass-message/script.js
+++ pass-message/script.js
@@ -1,7 +1,15 @@
 const message = document.getElementById("input-message");
 
+// Add a keydown event listener to the input element
+// This function will run every time a key is pressed while the input is focused
+message.addEventListener('keydown', function(event){
+  if(event.key == 'Enter'){
+    passMessage();
+  }
+});
+
+/// The function for displays the input message and clears the input field.
 function passMessage(){
   document.getElementById("display-message").innerText = message.value;
-  console.log(message);
   message.value = "";
 }
