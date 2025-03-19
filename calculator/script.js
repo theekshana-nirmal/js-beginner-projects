a6933a9e167571c71002bc067efae5b8f02a8753
Theekshana Nirmal <theekshanab1234@gmail.com>
2025-03-19 19:56:00 +0530

    Add comments to JavaScript functions for calculator operations

--- calculator/script.js
+++ calculator/script.js
@@ -1,18 +1,23 @@
+// Expression means the number that is being displayed on the screen
 var expression = '';
 let display = document.getElementById("display");
 
+
+// function to display the number on the screen
 function press(num){
   expression += num;
   display.value = expression;
   console.log(expression);
 }
 
+// function to erase the number on the screen
 function erase(){
   expression = "";
   display.value = expression;
   console.log(expression);
 }
 
+// function to calculate the expression
 function calc(){
   display.value = eval(expression);
   expression = '';
