dd7ad39b6bef0293a52852133c2b1e75d6d02853
Theekshana Nirmal <theekshanab1234@gmail.com>
2025-03-20 23:36:09 +0530

    Add comments to explain how the functions work

--- tip-calculator/script.js
+++ tip-calculator/script.js
@@ -3,6 +3,7 @@ const guests = document.getElementById("guest");
 const quality = document.getElementById("quality");
 const tipAmount = document.getElementById("tip-amount");
 
+// Arrow function to calculate the tip. It takes the amount, quality and number of guests and calculates the tip for each guest.
 calculate = () => {
   const tip = ((amount.value * quality.value) / guests.value).toFixed(2);
   amount.value = "";
@@ -18,6 +19,7 @@ calculate = () => {
   }
 }
 
+// Function to show the tip amount. Basically, it adds a class to the element and removes it after 5 seconds.
 showTipAmount = () => {
   var x = tipAmount;
   x.className = "show-tip";
