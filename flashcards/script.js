572f5d8289d108ba119c15a2e95bb322b22568be
Theekshana Nirmal <theekshanab1234@gmail.com>
2025-03-22 11:43:15 +0530

    Implement local storage functionality for flashcards and enable answer toggling on click

--- flashcards/script.js
+++ flashcards/script.js
@@ -1,11 +1,18 @@
-//TODO: Save flashcards to local storage
-//TODO: Load flashcards from local storage
 //TODO: Only show answer when clicked on the flashcard
 
 const create_box = document.getElementsByClassName("create-box")[0];
 const question = document.getElementById("question");
 const answer = document.getElementById("answer");
 var flashcards = document.getElementsByClassName("flashcards")[0];
+
+// Get flashcards from local storage and if there are none, create an empty array
+let contentArray = localStorage.getItem("flashcards")
+  ? JSON.parse(localStorage.getItem("flashcards"))
+  : [];
+
+// Create a new local storage item if there are no flashcards
+localStorage.setItem("flashcards", JSON.stringify(contentArray));
+getFlashcards();
 
 function showCreateBox() {
   create_box.style.display = "block";
@@ -23,6 +30,9 @@ function addFlashcard() {
   const q_text = question.value;
   const a_text = answer.value;
 
+  contentArray.push({ q: q_text, a: a_text });
+  localStorage.setItem("flashcards", JSON.stringify(contentArray));
+
   const card = document.createElement("div");
   card.className = "flashcard";
 
@@ -31,6 +41,11 @@ function addFlashcard() {
 
   q.textContent = q_text;
   a.textContent = a_text;
+
+  // Show answer when clicked on the flashcard
+  card.addEventListener("click", () => {
+    a.style.display = a.style.display === "block" ? "none" : "block";
+  });
 
   card.appendChild(q);
   card.appendChild(a);
@@ -44,4 +59,32 @@ function delFlashcards() {
     flashcards.innerHTML = "";
     flashcards.style.animation = "";
   }, 300);
+
+  localStorage.removeItem("flashcards");
+}
+
+// Get flashcards from local storage and display them
+function getFlashcards() {
+  const storedFlashcards = localStorage.getItem("flashcards");
+  contentArray = storedFlashcards ? JSON.parse(storedFlashcards) : [];
+  contentArray.forEach((content) => {
+    const card = document.createElement("div");
+    card.className = "flashcard";
+
+    const q = document.createElement("h3");
+    const a = document.createElement("p");
+
+    q.textContent = content.q;
+    a.textContent = content.a;
+
+    // Show answer when clicked on the flashcard
+    card.addEventListener("click", () => {
+      a.style.display = a.style.display === "block" ? "none" : "block";
+    });
+
+    card.appendChild(q);
+    card.appendChild(a);
+
+    flashcards.appendChild(card);
+  });
 }
