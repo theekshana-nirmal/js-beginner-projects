9b5512d58619a8b6cd2f035c652c791e6daf5b56
Theekshana Nirmal <theekshanab1234@gmail.com>
2025-04-05 12:42:53 +0530

    Implement image loading and removal functions in Unsplash Image Generator

--- unsplash-image-generator/script.js
+++ unsplash-image-generator/script.js
@@ -7,4 +7,32 @@ input.addEventListener('keydown', function (event){
   }
 });
 
-function loadImg(){}
+function loadImg(){
+  removeImg();
+  const url = "https://api.unsplash.com/search/photos?query=" + input.value + "per_page=9&client_id=Lg1KkuEkjZ1Th2UmhzE35xPydj2MCnefwn_rd3xE528";
+
+  fetch(url)
+  .then(response => {
+    if(response.ok){
+      return response.json();
+    }else{
+      alert("Error: " + response.status);
+    }
+  })
+  .then(data => {
+    const imageNodes = [];
+    for(let i = 0; i < data.results.length; i++){
+      imageNodes[i] = document.createElement("div");
+      imageNodes[i].className = "img";
+      imageNodes[i].style.backgroundImage = "url(" + data.results[i].urls.raw + ")";
+      imageNodes[i].addEventListener("dblclick", function(){
+        window.open(data.results[i].links.download, "_blank")
+      })
+      grid.appendChild(imageNodes[i]);
+    }})
+}
+
+
+function removeImg(){
+  grid.innerHTML = "";
+}
